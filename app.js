cfa942e49290026f4684dd816f44cbb806dfdefd
Steven Meads <sbmeads@gmail.com>
2017-03-15 23:52:33 -0700

    add css

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ $(document).ready(function() {
   $('.container').append("<div id='login-container'></div>");
   $('.container').append("<button type='submit' id='logout-btn' class='hide'>" + "Logout" + "</button>");
   $('.container').append("<button id='add-btn' class='hide'>" + "Add" + "</button>");
+  // Login Form
   $('#login-container').append("<form id='login-form'></form>");
   $('#login-form').append("<input type='email' id='email' placeholder='username'>");
   $('#login-form').append("<input type='password' id='password' placeholder='password'>");
   $('#login-form').append("<button type='submit' id='login-btn'>" + "Login" + "</button>");
   $('#login-form').append("<button type='submit' id='signup-btn'>" + "SignUp" + "</button>");
+  // BDday Form
   $(".container").append("<form class='hide' id='bday-form'></form>");
-  $("#bday-form").append("Name: " + "<input type='text' id='name'/>");
-  $("#bday-form").append("Email: " + "<input type='email' id='email-add'/>");
+  $("#bday-form").append("Name: " + "<input type='text' id='name' placeholder='name'/>");
+  $("#bday-form").append("Email: " + "<input type='email' id='email-add' placeholder='email'/>");
   $("#bday-form").append("Birthday: " + "<input type='date' id='birthday'/>");
   $("#bday-form").append("<button id='submit-btn'>" + "Submit" +"</button>");
-  $(".container").append("<div class='hide' id='contact-list'><h1>" + "BDAY CONTACT LIST" + "</h1></div>")
+  // BDay List
+  $(".container").append("<div class='hide' id='contact-list' style='border: 1px solid'><h1>" + "BDAY CONTACT LIST" + "</h1></div>")
 
   // On signup show ADD btn
   function signUp() {
-    $('#main-header').text("B-Day List");
+    $('#main-header').text("ADD");
     $('#login-container').hide();
     $('#logout-btn').removeClass('hide');
     $('#add-btn').removeClass('hide');
@@ -35,7 +38,7 @@ $(document).ready(function() {
 
   function logOut() {
     console.log("logged Out");
-    $('#main-header').text("Login/SignUp");
+    $('#main-header').text("REGARDS");
     $('#logout-btn').addClass('hide');
     $('#add-btn').addClass('hide');
     $('#bday-form').addClass('hide');
@@ -51,7 +54,7 @@ $(document).ready(function() {
   function addContact(name, email, birthday) {
     console.log(email);
     console.log("addcontact", uid);
-    const database = firebase.database().ref('/userContacts').child('/' + uid);
+    const database = firebase.database().ref('/' + uid);
     console.log("UID", uid);
     const contact = {
         name: name,
@@ -61,17 +64,23 @@ $(document).ready(function() {
 
     database.push(contact);
 
-    database.on('value', function(data) {
-      console.log(database);
-      console.log(data.val());
-      console.log(data.name);
-      console.log(data.birthday);
-      console.log(data.email);
+    database.on('child_added', function(data) {
+      let nameData = data.val().name;
+      let birthdayData = data.val().birthday;
+      let emailData = data.val().email;
+      console.log(nameData);
+      console.log(birthdayData);
+      console.log(emailData);
+      $('#contact-list').removeClass();
+      $('#contact-list').append("<p class='name' style='border: 1px solid><strong>" + "Name: " + "</strong>" + nameData + "</p>");
+      $('#contact-list').append("<p class='birthday'><strong>" + "Birthday: " + "</strong>" + birthdayData + "</p>");
+      $('#contact-list').append("<p class='email'><strong>" + "Email: " + "</strong>" + "<a href='#'>" + emailData + "</a></p>");
+
+    }, function (errorObject) {
+       console.log('The read failed: ' + errorObject.code);
+
     });
   }
-
-
-
   // Add login event
   $('#login-btn').on('click', function(event) {
     event.preventDefault();
@@ -118,7 +127,6 @@ $(document).ready(function() {
     firebase.auth().signOut();
     logOut();
   });
-
   // Add a realtime listener
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser) {
@@ -130,13 +138,13 @@ $(document).ready(function() {
       signUp();
     }
   })
-
+  // Add B-Day to contact list
   $('#add-btn').on('click', function(event) {
     event.preventDefault();
     console.log("clicked");
     addBday();
   });
-
+  // Add contact to database
   $('#submit-btn').on('click', function(event) {
     event.preventDefault();
     console.log("clicked submit btn");
